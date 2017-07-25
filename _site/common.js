4c9ea7f4445f6b17f957188ad1150545b8cd4e34
Bicheno <1135064577@qq.com>
2017-07-25 17:04:10 +0800

    添加common.js文件

---
+++ _site/common.js
@@ -0,0 +1,14 @@
+
+/* iframe自适应内容高度 */
+function setIframeHeight(iframe) {
+if (iframe) {
+var iframeWin = iframe.contentWindow || iframe.contentDocument.parentWindow;
+if (iframeWin.document.body) {
+iframe.height = iframeWin.document.documentElement.scrollHeight || iframeWin.document.body.scrollHeight;
+}
+}
+};
+
+window.onload = function () {
+setIframeHeight(document.getElementById('view_frame'));
+};
